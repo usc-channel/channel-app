86bfcc332068546b2bb90d7296c2ff649f6fe3bf
Kyle Roach <kroach.work@gmail.com>
2018-03-04 14:09:37 -0400

    Add offline status component

---
+++ src/components/Offline.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Image, Platform, StyleSheet, Text, View } from 'react-native'
+import { Button } from 'react-native-elements'
+
+import { Theme } from '@config'
+
+interface Props {
+  message?: string
+  retrying: boolean
+  action(): void
+}
+
+const Offline: React.SFC<Props> = ({ message, retrying, action }) => (
+  <View style={styles.container}>
+    <Image style={styles.image} source={require('../assets/tv-empty.png')} />
+
+    <Text style={styles.text}>
+      This might be easier with an internet connection.
+    </Text>
+
+    <Button
+      textStyle={{ color: Theme.primary }}
+      containerStyle={{ marginTop: 15 }}
+      clear
+      loading={retrying}
+      text={message}
+      loadingProps={{ size: 'small' }}
+      loadingStyle={{ marginVertical: 9 }}
+      onPress={action}
+    />
+  </View>
+)
+
+Offline.defaultProps = {
+  message: 'Try again',
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'relative',
+  },
+  text: {
+    color: 'rgba(0,0,0,0.87)',
+    fontSize: 18,
+    fontFamily: 'NunitoSans-SemiBold',
+    textAlign: 'center',
+  },
+  image: {
+    marginBottom: 16,
+    width: 196,
+    height: 162,
+    marginTop: Platform.OS === 'ios' ? -50 : -30,
+  },
+})
+
+export default Offline
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import NavIcon from './NavIcon'
 import Touchable from './Touchable'
 import PostMeta from './PostMeta'
+import Offline from './Offline'
 
-export { NavIcon, Touchable, PostMeta }
+export { NavIcon, Touchable, PostMeta, Offline }
