23cac0546316d6154a6944601a230a9d7f267c58
Kyle Roach <kroach.work@gmail.com>
2018-03-04 22:10:46 -0400

    Setup Media page tabs

--- src/containers/Media.container.ts
+++ src/containers/Media.container.ts
@@ -1,14 +1,56 @@
-import { StackNavigator } from 'react-navigation'
-import { Media } from '@pages'
+import { StackNavigator, TabNavigator } from 'react-navigation'
+import { Gallery, Videos } from '@pages'
 import { Theme } from '@config'
+
+const Tabs = TabNavigator(
+  {
+    Videos: {
+      screen: Videos,
+    },
+    Gallery: {
+      screen: Gallery,
+    },
+  },
+  {
+    ...TabNavigator.Presets.AndroidTopTabs,
+    tabBarPosition: 'top',
+    swipeEnabled: true,
+    tabBarOptions: {
+      style: {
+        backgroundColor: Theme.primary,
+      },
+      indicatorStyle: {
+        backgroundColor: '#fff',
+      },
+      labelStyle: {
+        fontFamily: 'NunitoSans-Bold',
+        fontSize: 14,
+      },
+    },
+  }
+)
 
 export default StackNavigator(
   {
     posts: {
-      screen: Media,
+      screen: Tabs,
+      navigationOptions: {
+        title: 'Media',
+      },
     },
   },
   {
-    navigationOptions: Theme.navigationOptions,
+    navigationOptions: {
+      ...Theme.navigationOptions,
+      headerStyle: {
+        ...Theme.navigationOptions.headerStyle,
+        borderBottomWidth: 0,
+      },
+      headerTitleStyle: {
+        fontFamily: 'NunitoSans-Bold',
+        fontSize: 20,
+        alignSelf: 'flex-start',
+      },
+    },
   }
 )
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,9 +1,21 @@
 import Posts from './Posts'
 import ViewPost from './ViewPost'
 import SearchPosts from './SearchPosts'
+
 import MarketPlace from './MarketPlace.page'
 import Lecturers from './Lecturers.page'
-import Media from './Media.page'
 import Archives from './Archives.page'
 
-export { Posts, MarketPlace, Lecturers, Media, Archives, ViewPost, SearchPosts }
+import Videos from './Videos'
+import Gallery from './Gallery'
+
+export {
+  Posts,
+  MarketPlace,
+  Lecturers,
+  Archives,
+  ViewPost,
+  SearchPosts,
+  Videos,
+  Gallery,
+}
