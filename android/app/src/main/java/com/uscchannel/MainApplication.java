164e696e116fa4b983e60232323ba5ba6218d924
Kyle Roach <kroach.work@gmail.com>
2018-04-07 00:38:51 -0400

    Temp remove linked items

--- android/app/src/main/java/com/uscchannel/MainApplication.java
+++ android/app/src/main/java/com/uscchannel/MainApplication.java
@@ -3,10 +3,6 @@ package com.uscchannel;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import org.wonday.pdf.RCTPdfView;
-import com.RNFetchBlob.RNFetchBlobPackage;
-import com.dylanvann.fastimage.FastImageViewPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,11 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RCTPdfView(),
-            new RNFetchBlobPackage(),
-            new FastImageViewPackage(),
-            new VectorIconsPackage()
+          new MainReactPackage()
       );
     }
 
