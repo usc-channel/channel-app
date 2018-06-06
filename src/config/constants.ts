6a2cc4d35aee8d4ffd3ad58ff9103524be22bd4e
Kyle Roach <kroach.work@gmail.com>
2018-06-05 20:12:53 -0400

    Fix localhost link on android

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -10,7 +10,7 @@ const getAPI = () => {
 
   return Platform.OS === 'ios'
     ? 'http://localhost:8000/api'
-    : 'http://192.168.1.104:8000/api'
+    : 'http://10.0.2.2:8000/api'
 }
 
 export const API = getAPI()
