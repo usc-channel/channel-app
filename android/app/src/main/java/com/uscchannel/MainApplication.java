4b94596b381a6820178ba6f807111ac672181a12
Kyle Roach <kroach.work@gmail.com>
2018-06-14 00:43:05 -0400

    Correctly link react-native-firebase

--- android/app/src/main/java/com/uscchannel/MainApplication.java
+++ android/app/src/main/java/com/uscchannel/MainApplication.java
@@ -3,6 +3,7 @@ package com.uscchannel;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import org.wonday.pdf.RCTPdfView;
 import com.RNFetchBlob.RNFetchBlobPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new LinearGradientPackage(),
             new RCTPdfView(),
             new RNFetchBlobPackage(),
