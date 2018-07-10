214a4be86f7f53e9ec8cb65a2e1bdb9a28fff971
Kyle Roach <kroach.work@gmail.com>
2018-07-10 02:12:14 -0400

    (archives) Get releases from API

--- src/pages/Releases/Releases.tsx
+++ src/pages/Releases/Releases.tsx
@@ -1,28 +1,61 @@
 import React from 'react'
-import { FlatList } from 'react-native'
+import { ActivityIndicator, FlatList } from 'react-native'
 import { NavigationScreenProps } from 'react-navigation'
 
-import releases from '../../releases.json'
 import ReleaseThumbnail from './components/ReleaseThumbnail'
 import { Release } from '@types'
+import { API } from '@config'
 
 type Props = NavigationScreenProps<{}>
 
-const Releases: React.SFC<Props> = props => {
-  const viewRelease = (release: Release) => {
-    props.navigation.navigate('ViewRelease', { release })
+interface State {
+  loading: boolean
+  releases: Release[]
+}
+
+class Releases extends React.Component<Props, State> {
+  state = {
+    loading: false,
+    releases: [],
   }
 
-  return (
-    <FlatList
-      data={releases}
-      keyExtractor={(a: Release) => a.title}
-      numColumns={2}
-      renderItem={({ item }) => (
-        <ReleaseThumbnail release={item} viewRelease={viewRelease} />
-      )}
-    />
-  )
+  componentDidMount() {
+    this.setState({ loading: true }, this.getReleases)
+  }
+
+  getReleases = async () => {
+    try {
+      const request = await fetch(`${API}/releases`)
+      const releases = await request.json()
+
+      this.setState({ releases, loading: false })
+    } catch (e) {
+      // tslint:disable-next-line:no-console
+      console.log(e)
+      // TODO: Show error message
+    }
+  }
+
+  viewRelease = (release: Release) => {
+    this.props.navigation.navigate('ViewRelease', { release })
+  }
+
+  render() {
+    const { loading, releases } = this.state
+
+    return loading ? (
+      <ActivityIndicator style={{ paddingVertical: 15 }} />
+    ) : (
+      <FlatList
+        data={releases}
+        keyExtractor={(a: Release) => a.title}
+        numColumns={2}
+        renderItem={({ item }) => (
+          <ReleaseThumbnail release={item} viewRelease={this.viewRelease} />
+        )}
+      />
+    )
+  }
 }
 
 export default Releases
