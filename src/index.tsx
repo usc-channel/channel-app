8345fb4e1803a312d8f27f03f2cb74188c56661a
Kyle Roach <kroach.work@gmail.com>
2018-08-01 19:33:49 -0400

    Remove yellowbox omitions

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,15 @@
-// tslint:disable:quotemark
-// tslint:disable:max-line-length
-
 import React from 'react'
-import { AppRegistry, Platform, StatusBar, View, YellowBox } from 'react-native'
+import { AppRegistry, StatusBar, View } from 'react-native'
 import { ApolloProvider } from 'react-apollo'
 import { Provider } from 'react-redux'
-import { createStackNavigator, NavigationScreenProps } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation'
 import { withNetworkConnectivity } from 'react-native-offline'
 
 import Tabs from './containers/Tabs'
 import Auth from './containers/Auth.container'
 import { graphqlClient, store, Theme } from '@config'
 import { NewCourse, NewLecturer, NewReview } from '@pages'
-import { NavIcon, Search } from '@components'
+import { Search } from '@components'
 
 let ModalStack = createStackNavigator(
   {
@@ -54,12 +51,6 @@ let ModalStack = createStackNavigator(
 ModalStack = withNetworkConnectivity({
   withRedux: true,
 })(ModalStack)
-
-YellowBox.ignoreWarnings([
-  'Warning: isMounted(...) is deprecated in plain JavaScript React classes.',
-  "Module RNFetchBlob requires main queue setup since it overrides `constantsToExport` but doesn't implement `requiresMainQueueSetup`. In a future release React Native will default to initializing all native modules on a background thread unless explicitly opted-out of.",
-  "Module RCTImageLoader requires main queue setup since it overrides `init` but doesn't implement `requiresMainQueueSetup`. In a future release React Native will default to initializing all native modules on a background thread unless explicitly opted-out of.",
-])
 
 const AppRoot = () => (
   <Provider store={store}>
