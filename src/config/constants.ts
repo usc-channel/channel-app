a46e09d3aadd18b17bdc2889a3a18ea3eba7d92a
Kyle Roach <kroach.work@gmail.com>
2018-09-04 08:13:24 -0400

    Change base api endpoint

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -10,8 +10,8 @@ const getAPI = () => {
   }
 
   return Platform.OS === 'ios'
-    ? 'http://localhost:8000/api'
-    : 'http://10.0.2.2:8000/api'
+    ? 'http://localhost:8000'
+    : 'http://10.0.2.2:8000'
 }
 
 const instance = Axios.create({
