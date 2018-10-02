b104de4ceeb600229d3013a00b116eecc5d436b0
Kyle Roach <kroach.work@gmail.com>
2018-10-02 16:22:06 -0400

    feat: Setup splashscreen on iOS

--- ios/USCChannel/AppDelegate.m
+++ ios/USCChannel/AppDelegate.m
@@ -10,6 +10,7 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "RNSplashScreen.h"
 
 @implementation AppDelegate
 
@@ -31,6 +32,8 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  [RNSplashScreen show];
   return YES;
 }
 
