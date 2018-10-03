3748638d725b911b37f0bc9ece16ecb7cfbee08f
Kyle Roach <kroach.work@gmail.com>
2018-10-02 20:43:27 -0400

    fix(SignIn): Signup button was outside safe area (iphone x)

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -4,6 +4,7 @@ import {
   Dimensions,
   LayoutAnimation,
   Platform,
+  SafeAreaView,
   StatusBar,
   StyleSheet,
   Text,
@@ -148,7 +149,7 @@ class SignIn extends React.Component<Props, State> {
 
   render() {
     return (
-      <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
         {Platform.OS === 'ios' && (
           <StatusBar barStyle="dark-content" animated />
         )}
@@ -254,7 +255,7 @@ class SignIn extends React.Component<Props, State> {
             </Touchable>
           </View>
         </KeyboardAwareScrollView>
-      </View>
+      </SafeAreaView>
     )
   }
 }
