5af641747e7e3d00b9ac91bdb1bd961c81de5d35
Kyle Roach <kroach.work@gmail.com>
2018-10-07 02:56:01 -0400

    refactor(ViewLecturer): Update reviews for new server pagination

--- src/pages/ViewLecturer/components/Reviews.tsx
+++ src/pages/ViewLecturer/components/Reviews.tsx
@@ -1,59 +1,97 @@
 import React from 'react'
 import { FlatList } from 'react-native'
+import { connect } from 'react-redux'
 
 import { Theme } from '@config'
-import { Review } from '@types'
+import {
+  DataOperation,
+  Dispatch,
+  LecturerReviewsState,
+  Review,
+  Store,
+} from '@types'
 import { Empty, Error, Spinner } from '@components'
+import { getLecturerReviews } from '@actions'
+
 import ReviewItem from './ReviewItem'
 
-interface Props {
-  reviews: Review[]
-  loading: boolean
-  error: boolean
-  refreshing: boolean
-  getReviews(): void
+interface ConnectedProps {
+  reviews: LecturerReviewsState
 }
 
-const Reviews: React.SFC<Props> = ({
-  reviews,
-  loading,
-  error,
-  getReviews,
-  refreshing,
-}) => (
-  <React.Fragment>
-    {loading && <Spinner />}
+interface ConnectedDispatch {
+  getLecturerReviews(lecturerId: number, operation?: DataOperation): void
+}
 
-    {!loading &&
-      error && (
+interface OwnProps {
+  lecturerId: number
+}
+
+type Props = ConnectedProps & OwnProps & ConnectedDispatch
+
+class Reviews extends React.Component<Props> {
+  componentDidMount() {
+    this.props.getLecturerReviews(this.props.lecturerId)
+  }
+
+  retry = () => {
+    this.props.getLecturerReviews(this.props.lecturerId, 'retry')
+  }
+
+  refresh = () => {
+    this.props.getLecturerReviews(this.props.lecturerId, 'refresh')
+  }
+
+  render() {
+    const { loading, error, results } = this.props.reviews
+
+    if (loading === 'fetch') {
+      return <Spinner />
+    }
+
+    if (error) {
+      return (
         <Error
           message="There's been a problem getting the reviews for this Lecturer."
-          loading={refreshing}
-          action={{ message: 'Try again', callback: getReviews }}
+          loading={loading === 'retry'}
+          action={{ message: 'Try again', callback: this.retry }}
         />
-      )}
+      )
+    }
 
-    {!loading &&
-      !error &&
-      reviews.length === 0 && (
+    if (results.length === 0) {
+      return (
         <Empty
           title="No Reviews"
           image={require('../../../assets/chat.png')}
           message="As reviews are added for this lecturer they'll appear here."
         />
-      )}
+      )
+    }
 
-    {!loading &&
-      !error &&
-      reviews.length > 0 && (
-        <FlatList
-          data={reviews}
-          keyExtractor={(review: Review) => review.id.toString()}
-          contentContainerStyle={{ backgroundColor: Theme.background }}
-          renderItem={({ item }) => <ReviewItem review={item} />}
-        />
-      )}
-  </React.Fragment>
-)
+    return (
+      <FlatList
+        data={results}
+        refreshing={loading === 'refresh'}
+        onRefresh={this.refresh}
+        keyExtractor={(review: Review) => review.id.toString()}
+        contentContainerStyle={{ backgroundColor: Theme.background }}
+        renderItem={({ item }) => <ReviewItem review={item} />}
+      />
+    )
+  }
+}
 
-export default Reviews
+const mapStateToProps = (state: Store) => ({
+  reviews: state.lecturerReviews,
+})
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  getLecturerReviews: (lecturerId: number, operation?: DataOperation) =>
+    dispatch(getLecturerReviews(lecturerId, operation)),
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Reviews)
