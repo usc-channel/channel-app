d8af897bd4633a7ad6109acbe87bc84a25f8c47b
Kyle Roach <kroach.work@gmail.com>
2018-10-15 03:01:22 -0400

    fix(Posts): Don't display chevron if no categories
    Don't display chevron next to post date if the post isn't assigned to
    category

--- src/components/PostMeta.tsx
+++ src/components/PostMeta.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import FastImage from 'react-native-fast-image'
+import { decode } from 'he'
+import moment from 'moment'
+
 import { Theme } from '@config'
 import { Author as AuthorModel, Category } from '@types'
 import { printArray } from '@util'
-import { decode } from 'he'
-import moment from 'moment'
-import FastImage from 'react-native-fast-image'
 
 interface Props {
   author: AuthorModel
@@ -13,38 +14,48 @@ interface Props {
   categories: Category[]
 }
 
-const PostMeta: React.SFC<Props> = ({ author, date, categories }) => (
-  <View style={styles.container}>
-    <View style={styles.avatarContainer}>
-      <FastImage
-        style={styles.avatar}
-        source={{
-          uri: author.avatar.url,
-        }}
-      />
-    </View>
+const PostMeta: React.SFC<Props> = ({ author, date, categories }) => {
+  const hasCategories = categories.length > 0
 
-    <View>
-      <Text style={styles.metaField}>{author.name}</Text>
-      <View style={styles.meta}>
-        <Text style={styles.metaField}>
-          {moment(date).isBefore(
-            moment(`${new Date().getFullYear()}-01-01`, 'YYYY-MM-DD')
-          )
-            ? moment(date).format('MMM D, YYYY')
-            : moment(date).format('MMM D')}
-        </Text>
+  return (
+    <View style={styles.container}>
+      <View style={styles.avatarContainer}>
         <FastImage
-          style={styles.arrow}
-          source={require('../assets/arrow.png')}
+          style={styles.avatar}
+          source={{
+            uri: author.avatar.url,
+          }}
         />
-        <Text style={[styles.metaField, styles.category]}>
-          {printArray(categories.map(a => decode(a.name)))}
-        </Text>
+      </View>
+
+      <View>
+        <Text style={styles.metaField}>{author.name}</Text>
+        <View style={styles.meta}>
+          <Text style={styles.metaField}>
+            {moment(date).isBefore(
+              moment(`${new Date().getFullYear()}-01-01`, 'YYYY-MM-DD')
+            )
+              ? moment(date).format('MMM D, YYYY')
+              : moment(date).format('MMM D')}
+          </Text>
+
+          {hasCategories && (
+            <FastImage
+              style={styles.arrow}
+              source={require('../assets/arrow.png')}
+            />
+          )}
+
+          {hasCategories && (
+            <Text style={[styles.metaField, styles.category]}>
+              {printArray(categories.map(a => decode(a.name)))}
+            </Text>
+          )}
+        </View>
       </View>
     </View>
-  </View>
-)
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
